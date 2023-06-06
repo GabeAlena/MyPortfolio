4812fd0d65f375d398cc971114d467c2d441e249
Alena Bazhukova <gabeaalena@gmail.com>
2023-06-06 16:00:24 +0500

    style(main/about): height for main page

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -8,7 +8,7 @@ function About() {
             <div className="about__description-image">
                 <img className="about__image" src={catMain} alt='here you could see the working cat'></img>
                 <p className="about__description">
-                    &#128161; What can you see? Here is a site-portfolio where you can post information about you,
+                    &#8226; What can you see? Here is a site-portfolio where you can post information about you,
                     your life, education, hobbies, career, various awards, any interesting news. Or you
                     can look through the profiles because my site is not ready yet.
                 </p>
@@ -22,19 +22,19 @@ function About() {
 
                 <div className="about__description_job-sites">
                     <a href="https://hh.kz" target="_blank" className="about__description_job-site">
-                        &#10057; HeadHunter (for post-Soviet countries)
+                        &#8226; HeadHunter (for post-Soviet countries)
                     </a>
                     <a href="https://suberjob.kz" target="_blank" className="about__description_job-site">
-                        &#10057; SuperJob (for post-Soviet countries)
+                        &#8226; SuperJob (for post-Soviet countries)
                     </a>
                     <a href="https://layboard.com" target="_blank" className="about__description_job-site">
-                        &#10057; LAYBOARD (for all countries)
+                        &#8226; LAYBOARD (for all countries)
                     </a>
                     <a href="https://indeed.com" target="_blank" className="about__description_job-site">
-                        &#10057; Indeed (for all countries)
+                        &#8226; Indeed (for all countries)
                     </a>
                     <a href="https://idealist.org" target="_blank" className="about__description_job-site">
-                        &#10057; idealist (for all countries)
+                        &#8226; idealist (for all countries)
                     </a>
                 </div>
             </p>
